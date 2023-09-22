06b401742ab087db1b3c761b3ed17e76bc305dec
Siddhu Warrier <siwarrie@cisco.com>
2023-09-22 18:44:16 +0100

    Remove local provisioner using aws cli because devnet labs env does not have awscli installed

--- modules/asav/post_provisioning.tf
+++ modules/asav/post_provisioning.tf
@@ -1,17 +1,4 @@
-resource "null_resource" "wait_for_asa_instance_to_be_ready" {
-  # If an IP changes, these scripts will run again
-  triggers = {
-    nodes_ips = aws_instance.asav.private_ip
-  }
-
-  provisioner "local-exec" {
-    command = "aws ec2 wait instance-status-ok --instance-ids ${aws_instance.asav.id} --region ${var.aws_region}"
-  }
-}
-
-
 resource "null_resource" "wait_for_asa_to_finish_booting" {
-  depends_on = [null_resource.wait_for_asa_instance_to_be_ready]
   # If an IP changes, these scripts will run again
   triggers = {
     nodes_ips = aws_instance.asav.private_ip
