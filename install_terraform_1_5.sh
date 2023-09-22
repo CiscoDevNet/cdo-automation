dab4b52209c044bff75993f5d24afd7f78e8cc05
Siddhu Warrier <siwarrie@cisco.com>
2023-09-22 20:47:33 +0100

    Install wget before trying to wget terraform

--- install_terraform_1_5.sh
+++ install_terraform_1_5.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 cd /tmp
+sudo apt-get update && sudo apt-get install -y wget
 wget -O terraform.zip https://releases.hashicorp.com/terraform/1.5.7/terraform_1.5.7_linux_amd64.zip
 unzip terraform.zip
 sudo mv terraform /usr/bin
